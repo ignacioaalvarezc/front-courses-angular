18b6f99b98a4cbe91b22d7b0516f25be5c11bfdb
ignacio <ignacioaalvarezc@gmail.com>
2023-11-29 12:44:38 -0300

    Generacion del servicio categoria

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baserUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http:HttpClient) { }
+
+  public listCategories(){
+    return this.http.get(`${baserUrl}/category`)
+  }
+}
